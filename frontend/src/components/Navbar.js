8b666dba7ef50d13011840103d663bc891ce7839
faizfazdlih <faizfazdlih8@gmail.com>
2026-05-08 23:04:32 +0700

    edit navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -274,7 +274,7 @@ export const Navbar = () => {
                 <span style={{ color: '#fff', fontSize: '0.7rem', fontWeight: 700 }}>!</span>
               </div>
               <span className="logo-text">
-                TERAS<span> WARGA</span>
+                TERAS WARGA<span>WLE</span>
               </span>
             </Link>
 
